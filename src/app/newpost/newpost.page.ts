9bce73e5548e4785a367eac559a1786397b26fc3
denielleoliva <denielle.oliva@gmail.com>
2022-04-16 23:34:31 -0700

    final push of night

--- src/app/newpost/newpost.page.ts
+++ src/app/newpost/newpost.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-newpost',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newpost.page.scss'],
 })
 export class NewpostPage implements OnInit {
+  timeLeft: number = 300;
+  interval;
+  subscribeTimer: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  
+  observableTimer() {
+    const source = timer(1000, 2000);
+    const abc = source.subscribe(val => {
+      console.log(val, '-');
+      this.subscribeTimer = this.timeLeft - val;
+    });
   }
 
 }
