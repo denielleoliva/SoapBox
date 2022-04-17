f220861771da042bd63873b5616af7dbfac1cc64
denielleoliva <denielle.oliva@gmail.com>
2022-04-17 11:13:29 -0700

    fixed thing

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {Router} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,26 +12,33 @@ export class LoginPage implements OnInit {
   ionicForm: FormGroup;
   isSubmitted = false;
 
-  constructor(public formBuilder: FormBuilder) { }
+  constructor(public formBuilder: FormBuilder,private route:Router, private http: HttpClient) { }
 
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.edu')]],
       password: ['', [Validators.required, Validators.minLength(6)]]
-    })
+    });
   }
 
   get errorControl() {
     return this.ionicForm.controls;
   }
 
-  submitForm() {
+  submitForm(postData:any) {
     this.isSubmitted = true;
     if (!this.ionicForm.valid) {
-      console.log('Please provide all the required values!')
+
+      var form = postData;
+
+      this.http.post("localhost:5154/login", form).subscribe((response)=>{
+        console.log(response);
+      });
+
+      this.route.navigate(['/tabnav']);
       return false;
     } else {
-      console.log(this.ionicForm.value)
+      console.log(this.ionicForm.value);
     }
   }
 
--- src/app/newpost/newpost.page.ts
+++ src/app/newpost/newpost.page.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-newpost',
   templateUrl: './newpost.page.html',
   styleUrls: ['./newpost.page.scss'],
 })
+
 export class NewpostPage implements OnInit {
   timeLeft: number = 300;
   interval;
   subscribeTimer: any;
 
   constructor() { }
+  
 
   ngOnInit() {
+    var obj;
+    fetch('./assets/dummy.json').then(res => res.json())
+    .then(json => {
+      obj = json;
+  });
+
   }
 
-  
-  observableTimer() {
-    const source = timer(1000, 2000);
-    const abc = source.subscribe(val => {
-      console.log(val, '-');
-      this.subscribeTimer = this.timeLeft - val;
-    });
-  }
 
 }
